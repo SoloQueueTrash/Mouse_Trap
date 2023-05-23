37307ede04d0ed57803cb2f06488e839bdfc9634
Tomás Morgado Vicente <up201904609@fe.up.pt>
2023-05-23 18:44:33 +0100

    Load traps from storage.

--- app/lib/trap_list.dart
+++ app/lib/trap_list.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:app/trap.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TrapListScreen extends StatefulWidget {
   const TrapListScreen({Key? key}) : super(key: key);
@@ -10,6 +13,23 @@ class TrapListScreen extends StatefulWidget {
 
 class _TrapListScreenState extends State<TrapListScreen> {
   List<Trap> _traps = [];
+
+  void _loadTraps() {
+    SharedPreferences.getInstance().then((prefs) {
+      var trapsJson = prefs.getStringList('traps');
+      if (trapsJson == null) {
+        return;
+      }
+      var traps = trapsJson.map((e) => Trap.fromJson(jsonDecode(e))).toList();
+      setState(() => _traps = traps);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTraps();
+  }
 
   @override
   Widget build(BuildContext context) {
