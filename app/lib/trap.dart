c81581392c3aad99d72f2b799a6649534bf5db10
Tomás Morgado Vicente <up201904609@fe.up.pt>
2023-05-23 18:08:19 +0100

    Trap (un)serialization.

--- app/lib/trap.dart
+++ app/lib/trap.dart
@@ -4,4 +4,18 @@ class Trap {
   int port;
 
   Trap({required this.name, required this.ip, required this.port});
+
+  factory Trap.fromJson(Map<String, dynamic> json) {
+    return Trap(
+      name: json['name'],
+      ip: json['ip'],
+      port: json['port'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'ip': ip,
+        'port': port,
+      };
 }
