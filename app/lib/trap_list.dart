4b25ae6e0833d10224ffdb7c3f03afb3b392e3b6
Tomás Morgado Vicente <up201904609@fe.up.pt>
2023-05-23 18:39:09 +0100

    Trap List Screen definition.

---
+++ app/lib/trap_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TrapListScreen extends StatefulWidget {
+  const TrapListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TrapListScreen> createState() => _TrapListScreenState();
+}
+
+class _TrapListScreenState extends State<TrapListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
