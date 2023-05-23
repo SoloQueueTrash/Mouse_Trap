1465d7c5a804fc82c670914fc606f122d0ee3f9c
Tomás Morgado Vicente <up201904609@fe.up.pt>
2023-05-23 20:39:37 +0100

    Build trap item.

--- app/lib/trap_list.dart
+++ app/lib/trap_list.dart
@@ -32,6 +32,66 @@ class _TrapListScreenState extends State<TrapListScreen> {
     });
   }
 
+  Widget _buildTrapItemContent(Trap trap, TrapStatus status) {
+    var textColor = Theme.of(context).brightness == Brightness.light
+        ? Colors.black
+        : Colors.white;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              trap.name,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: textColor,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              '${trap.ip}:${trap.port}',
+              style: TextStyle(fontSize: 14, color: textColor.withOpacity(0.7)),
+            ),
+          ],
+        ),
+        IconButton(
+          icon: Icon(status.icon),
+          color: status.color,
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+
+  Widget _buildTrapItem(BuildContext context, int index) {
+    var trap = _traps[index];
+    return FutureBuilder(
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Padding(
+              padding: EdgeInsets.all(16),
+              child: Center(child: CircularProgressIndicator()),
+            );
+          } else {
+            var status = snapshot.data as TrapStatus;
+            return Container(
+              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              decoration: BoxDecoration(
+                color: status.color.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(16),
+                border: Border.all(color: status.color, width: 2),
+              ),
+              child: _buildTrapItemContent(trap, status),
+            );
+          }
+        },
+        future: trap.getStatus());
+  }
+
   @override
   void initState() {
     super.initState();
