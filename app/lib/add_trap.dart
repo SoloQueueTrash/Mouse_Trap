520882739582ab5c44e52e0e3955565db3d8dae9
Tomás Morgado Vicente <up201904609@fe.up.pt>
2023-05-23 23:24:34 +0100

    New trap form name field.

--- app/lib/add_trap.dart
+++ app/lib/add_trap.dart
@@ -7,9 +7,27 @@ class AddTrapScreen extends StatefulWidget {
 
 class _AddTrapScreenState extends State<AddTrapScreen> {
   final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+
+  TextFormField _buildNameField() {
+    return TextFormField(
+      controller: _nameController,
+      decoration: const InputDecoration(
+        labelText: 'Name',
+        border: OutlineInputBorder(),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter a name';
+        }
+        return null;
+      },
+    );
+  }
 
   @override
   void dispose() {
+    _nameController.dispose();
     super.dispose();
   }
 
@@ -22,6 +40,7 @@ class _AddTrapScreenState extends State<AddTrapScreen> {
         child: ListView(
           padding: const EdgeInsets.all(16),
           children: [
+            _buildNameField(),
           ],
         ),
       ),
