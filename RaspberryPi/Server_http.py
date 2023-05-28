63f600ff4f65b17fbbbbb87fdc720a21666c2811
SoloQueueTrash <escola.pedro.cunha@gmail.com>
2023-05-28 08:49:06 +0100

    nothing to report lel

--- RaspberryPi/Server_http.py
+++ RaspberryPi/Server_http.py
@@ -5,12 +5,19 @@ from datetime import datetime
 import picamera
 import time
 import serial
+import requests
+import sys 
 
 closed = True
 picture_path = None
+clients = []
+recentPhoto = ""
+ 
+api_key = 'AAAAMHMssHI:APA91bEW7enKmz6XH0FtAUPvce7ggJTLCF--QQiFnNw43lyIGAmXnBMfptSKqHxSoTGYkLB5ncQE36ko9WqWdbMQQ7iKtmNXh3lmms7V1_X6eGSHucMvaQai6ejD0_9NppPoddKeXUrd'
+
 
 # Create Arduino connection
-arduino = serial.Serial("/dev/ttyACM0", 9600, timeout = 1)
+arduino = None #serial.Serial("/dev/ttyACM1", 9600)
 
 app = Flask(__name__)
 
@@ -30,9 +37,11 @@ def toggle():
     return jsonify({'status': 'ok'})
 
 
-@app.route('/status')
+@app.route('/hello')
 def status():
-    return jsonify({'status': 'closed' if closed else 'open'})
+    client_ip = request.remote_addr
+    clients.append(client_ip)
+    return jsonify({'status': 'ok' , 'message' : 'Hello'})
 
 # Sends commands to Arduino
 @app.route('/sendCommand/<string:message>')
@@ -53,10 +62,19 @@ def sendToArduino(message):
         path = 'images/' + filename
 
         save_image(image, path)
+        
+        recentPhoto = path
 
         write_logs(f'File: {filename} sent to {client_ip}')
         
         return send_file(io.BytesIO(image), mimetype='image/jpeg')
+    elif(message == 'cmd_recentPhoto'):
+        if(recentPhoto == ''):
+            return jsonify({'status': '404', 'message': 'Photo Not Found'})
+        else:
+            file = open(recentPhoto, 'rb')
+            imageB = file.read()
+            return send_file(io.BytesIO(imageB), mimetype='image/jpeg')
     else:
         arduino.flush() # flush any existing data in the input buffer
         time.sleep(0.1) #wait for serial to opens
@@ -74,8 +92,8 @@ def sendToArduino(message):
 
         write_logs(f'Command \'{answer}\' returned from Arduino')
 
-        # returns "Not a valid command" if the command is wrong
         return jsonify({'status': 'ok', 'message': answer})
+
 
 def save_image(image, path):
     file = open(path, 'wb')
@@ -88,5 +106,42 @@ def write_logs(message):
     file.write('[' + str(now) + ']' +': '+ message + '\n')
     file.close()
 
+def handle_detected():
+    while(True):
+        while arduino.inWaiting() == 0: pass
+
+        if  arduino.inWaiting() > 0: 
+            command = arduino.readline().decode().replace('\r','').replace('\n','')
+            arduino.flushInput() #remove data after reading
+
+            print("comand = " + command)
+            if(command == "cmd_detected"):
+                write_logs(f'Command \'{command}\' got from Arduino')
+                print(jsonify({'status': 'ok', 'message': 'cmd_detect'}))
+                requests.post(
+                    'https://fcm.googleapis.com/v1/projects/myproject-b5ae1/messages:send',
+                    headers={
+                        'Content-Type': 'application/json',
+                        'Authorization': f'Bearer {api_key}'
+                    },
+                    json={
+                        "message": {
+                            "topic": "foo-bar",
+                            "notification": {
+                                "body": "This is a Firebase Cloud Messaging Topic Message!",
+                                "title": "FCM Message"
+                            }
+                        }
+                    }
+                )
+
+import threading
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+
+    arduino = serial.Serial(sys.argv[1], 9600)
+
+    thread = threading.Thread(target=handle_detected)
+    thread.deamon =True
+    thread.start()
+
+    app.run(host='0.0.0.0', port=8308, debug=True)
