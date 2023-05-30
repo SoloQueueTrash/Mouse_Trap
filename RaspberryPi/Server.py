0e35dd90210894af17f4e684328ba88e91345d11
Tomás Morgado Vicente <up201904609@fe.up.pt>
2023-05-30 02:07:35 +0100

    Reformat server.

--- RaspberryPi/Server.py
+++ RaspberryPi/Server.py
@@ -1,19 +1,20 @@
+import io
 import os
+import sys
+import threading
+import time
+from datetime import datetime
+
+import picamera
+import serial
 from flask import Flask
 from flask import jsonify, send_file, request
-import io
-from datetime import datetime
-import picamera
-import time
-import serial
-import requests
-import sys 
-import threading
 
 global status
 status = 'cmd_open'
 
 app = Flask(__name__)
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -22,19 +23,21 @@ def not_found(error):
     write_logs(f'Wrong Route: \'{path}\' requested from {client_ip}')
     return jsonify({'status': '404', 'message': 'Route Not Found'})
 
+
 @app.route('/status')
 def status():
     client_ip = request.remote_addr
     global status
     write_logs(f'Status: {status} requested from {client_ip}')
     return jsonify({'status': 'OK', 'message': status})
-    
+
+
 @app.route('/photo/<string:message>')
-def photoHanler(message):
+def photoHandler(message):
     client_ip = request.remote_addr
     write_logs(f'Command: \'{message}\' requested from {client_ip}')
 
-    if(message == 'cmd_photo'):
+    if message == 'cmd_photo':
         stream = io.BytesIO()
 
         with picamera.PiCamera() as camera:
@@ -43,16 +46,17 @@ def photoHanler(message):
 
         image = stream.getvalue()
         timestamp = datetime.now()
-        filename = str(timestamp.day) + '-' +  str(timestamp.month) + '-' + str(timestamp.year) + '_'+ str(timestamp.hour) + ':' + str(timestamp.minute) + ':' + str(timestamp.second) + '.jpeg'
+        filename = str(timestamp.day) + '-' + str(timestamp.month) + '-' + str(timestamp.year) + '_' + str(
+            timestamp.hour) + ':' + str(timestamp.minute) + ':' + str(timestamp.second) + '.jpeg'
         path = 'images/' + filename
 
         save_image(image, path)
         write_logs(f'File: {filename} sent to {client_ip}')
-        
+
         return send_file(io.BytesIO(image), mimetype='image/jpeg')
-    elif(message == 'cmd_recent'):
+    elif message == 'cmd_recent':
         fileA = get_newest_file("images/")
-        if(fileA is None):
+        if fileA is None:
             return jsonify({'status': '404', 'message': 'Photo Not Found'})
         else:
             file = open("images/" + fileA, 'rb')
@@ -61,17 +65,18 @@ def photoHanler(message):
     else:
         return jsonify({'status': '404', 'message': 'Invalid Command'})
 
+
 # Sends commands to Arduino
 @app.route('/arduino/<string:message>')
 def toArduino(message):
     client_ip = request.remote_addr
     write_logs(f'Command: \'{message}\' requested from {client_ip}')
-    if(message == "cmd_open" or message == "cmd_close"):
+    if message == "cmd_open" or message == "cmd_close":
         global status
         status = message
         print("CURRET STATUS = " + status)
-        arduino.flush() 
-        time.sleep(0.1) 
+        arduino.flush()
+        time.sleep(0.1)
 
         if arduino.isOpen():
             arduino.write(message.encode())
@@ -80,6 +85,7 @@ def toArduino(message):
     else:
         return jsonify({'status': '404', 'message': 'Invalid Command'})
 
+
 def get_newest_file(folder_path):
     files = os.listdir(folder_path)
     if not files:
@@ -87,43 +93,47 @@ def get_newest_file(folder_path):
 
     files.sort(key=lambda x: os.path.getmtime(os.path.join(folder_path, x)))
 
-    newest_file = files[-1] 
+    newest_file = files[-1]
     return newest_file
+
 
 def save_image(image, path):
     file = open(path, 'wb')
     file.write(image)
     file.close()
 
+
 def write_logs(message):
     file = open('logs/accesses.log', 'a')
     now = datetime.now()
-    file.write('[' + str(now) + ']' +': '+ message + '\n')
+    file.write('[' + str(now) + ']' + ': ' + message + '\n')
     file.close()
 
+
 def handle_detected():
-    while(True):
+    while True:
         while arduino.inWaiting() == 0: pass
 
-        if  arduino.inWaiting() > 0: 
+        if arduino.inWaiting() > 0:
             command = arduino.readline()
-            arduino.flushInput() #remove data after reading
+            arduino.flushInput()  # remove data after reading
 
-        command = command.decode().replace('\r','').replace('\n','')
+        command = command.decode().replace('\r', '').replace('\n', '')
 
         write_logs(f'Command \'{command}\' got from Arduino')
         print("COMMAND = " + command)
 
-        if(command == "cmd_detected"):
+        if command == "cmd_detected":
             print("DETECTED")
-        if(command == "cmd_autoclose"):
+        if command == "cmd_autoclose":
             global status
             status = 'cmd_close'
             print("AUTOCLOSE")
 
+
 arduino = serial.Serial(sys.argv[1], 9600)
 thread = threading.Thread(target=handle_detected)
-thread.deamon =True
+thread.deamon = True
 thread.start()
 
 if __name__ == '__main__':
